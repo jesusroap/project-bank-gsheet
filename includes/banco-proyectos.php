f69a981c173c3814eff30031c7566dc3f95d9143
Apoyo Editorial <apoyoeditorial@iMac-de-Apoyo.local>
2021-05-26 11:41:05 -0500

    Modificacion de animacion carga de proyectos

--- includes/banco-proyectos.php
+++ includes/banco-proyectos.php
@@ -2,6 +2,7 @@
 echo
 "<div id='contenedor_carga'>
     <div id='carga'></div>
+    <div class='contenedor-logo'><img src='https://uninavarra.edu.co/wp-content/themes/edubase-uninavarra/images/logo.png' alt='Uninavarra'></div>
 </div>
 <table id='tabla_dt' class='display'>
     <thead>
