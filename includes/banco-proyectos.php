30e17861f3afd99cef9173dd0d9063864f8310b4
Mauricio Roa <mauricioroa@iMac-de-Mauricio.local>
2021-08-05 16:37:50 -0500

    Animacion cargando en header de la tabla

--- includes/banco-proyectos.php
+++ includes/banco-proyectos.php
@@ -3,7 +3,12 @@ echo
 "<table id='tabla_dt' class='display'>
     <thead>
         <tr>
-            <th></th>
+            <th>
+                <div id='contenedor_carga'>
+                    <div id='carga'></div>
+                    <div class='contenedor-logo'><img src='https://uninavarra.edu.co/wp-content/themes/edubase-uninavarra/images/logo.png' alt='Uninavarra'></div>
+                </div>
+            </th>
             <th></th>
             <th></th>
             <th></th>
